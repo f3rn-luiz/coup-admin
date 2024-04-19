1438144822944615a0079564ee0ad454ea2364ae
Luiz Fernando <f3rn.luiz@gmail.com>
2024-04-18 23:08:21 -0300

    feat(Route / Login / Home): configuração do route, finalização do login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-	{
-		path: 'home',
-		loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-	},
-	{
-		path: '',
-		redirectTo: 'home',
-		pathMatch: 'full',
-	},
+	{ path: '', loadComponent: () => import('./pages/login/login.page').then((p) => p.LoginPage) },
+	{ path: 'home', loadComponent: () => import('./pages/home/home.page').then((p) => p.HomePage) },
 ];
---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon } from '@ionic/angular/standalone';
+
+@Component({
+	selector: 'app-login',
+	templateUrl: 'login.page.html',
+	standalone: true,
+	imports: [IonIcon, IonHeader, IonToolbar, IonTitle, IonContent, ReactiveFormsModule],
+})
+export class LoginPage implements OnInit {
+	senhaForm!: UntypedFormGroup;
+
+	constructor(
+		private _formBuilder: UntypedFormBuilder,
+		private _router: Router,
+	) {}
+
+	ngOnInit(): void {
+		this.senhaForm = this._formBuilder.group({
+			senha: [null],
+		});
+	}
+
+	entrarAplicativo() {
+		if (this.senhaForm.value.senha !== '12345') this.senhaForm.get('senha')?.setValue(null);
+		else this._router.navigateByUrl('home');
+	}
+}
