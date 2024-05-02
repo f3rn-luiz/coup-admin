3640dc26266baa5397991ba660508ac5d1c91e35
Luiz Fernando <f3rn.luiz@gmail.com>
2024-05-02 12:32:12 -0300

    docs(Login -> Home): alteração de abordagem e pré configuração da home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-	{ path: '', loadComponent: () => import('./pages/login/login.page').then((p) => p.LoginPage) },
+	{ path: '', loadComponent: () => import('./pages/home/home.page').then((p) => p.LoginPage) },
 	{ path: 'config', loadComponent: () => import('./pages/config/config.page').then((p) => p.ConfigPage) },
 	{ path: 'players', loadComponent: () => import('./pages/players/players.page').then((p) => p.PlayersPage) },
 	{ path: 'game', loadComponent: () => import('./pages/game/game.page').then((p) => p.GamePage) },
---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { GameService } from 'src/app/core/game.service';
+
+@Component({
+	selector: 'app-login',
+	templateUrl: 'home.page.html',
+	standalone: true,
+	imports: [IonicModule],
+})
+export class LoginPage {
+	texto_rodape = '';
+
+	constructor(
+		private _gameService: GameService,
+		private _router: Router,
+	) {
+		this.texto_rodape = this._gameService.texto_rodape;
+	}
+
+	entrarAplicativo() {
+		this._router.navigateByUrl('config');
+	}
+}
