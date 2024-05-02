a30542d50c622874dee2c60cda702614937abd81
Luiz Fernando <f3rn.luiz@gmail.com>
2024-05-02 10:53:42 -0300

    docs(Nome / Versão / Projeto): Alteração do nome e versão, organização do projeto
    BREAKING CHANGE: ]

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,20 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-	appId: 'com.luizfsouza.coupsimplecounter',
-	appName: 'COUP Simple Counter',
+	appId: 'com.luizfsouza.coupadmin',
+	appName: 'COUP Admin',
 	webDir: 'www',
+	plugins: {
+		SplashScreen: {
+			launchShowDuration: 3000,
+			backgroundColor: '#000000',
+			splashFullScreen: true,
+			splashImmersive: true,
+			showSpinner: true,
+			androidSpinnerStyle: 'small',
+			iosSpinnerStyle: 'small',
+		},
+	},
 };
 
 export default config;
