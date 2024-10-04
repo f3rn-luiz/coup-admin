f7c3d1c6c73a36df1d541dc92aef987c3274236e
Luiz Fernando <f3rn.luiz@gmail.com>
2024-10-04 11:33:22 -0300

    refactor(Config / Players): separação da tela dos jogadores das configurações

--- src/app/pages/config/config.page.ts
+++ src/app/pages/config/config.page.ts
@@ -1,67 +1,42 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { Subject, takeUntil } from 'rxjs';
 import { GameService } from 'src/app/core/game.service';
-import { Jogador } from 'src/app/core/game.type';
 
 @Component({
 	selector: 'app-config',
 	templateUrl: 'config.page.html',
 	standalone: true,
-	changeDetection: ChangeDetectionStrategy.OnPush,
 	imports: [IonicModule, CommonModule, ReactiveFormsModule],
 })
 export class ConfigPage implements OnInit, OnDestroy {
 	private _unsubscribeAll: Subject<any> = new Subject<any>();
 
-	texto_rodape = '';
-
-	configuracao1Form!: UntypedFormGroup;
-	configuracao2Form!: UntypedFormGroup;
-
-	fase_conf = 0;
-	jogadores: Jogador[] | null = [];
+	configuracaoForm!: UntypedFormGroup;
 
 	constructor(
-		private _changeDetectorsRef: ChangeDetectorRef,
 		private _formBuilder: UntypedFormBuilder,
 		private _route: Router,
 		private _gameService: GameService,
-	) {
-		_gameService._jogadores.pipe(takeUntil(this._unsubscribeAll)).subscribe({ next: (j) => (this.jogadores = j) });
-		this.texto_rodape = _gameService.texto_rodape;
-	}
+	) {}
 
 	ngOnInit(): void {
-		this.configuracao1Form = this._formBuilder.group({
-			numero_jogadores: [0, [Validators.min(2), Validators.max(10)]],
+		this.configuracaoForm = this._formBuilder.group({
+			numero_jogadores: [0, [Validators.min(2)]],
 			numero_vidas: [0, [Validators.min(1)]],
 			dinheiro_inicial: [0, [Validators.min(0)]],
 			custo_golpe_estado: [0, [Validators.min(0)]],
 			golpe_estado_obrigatorio: [0, [Validators.min(0)]],
 		});
 
-		this._gameService._numero_jogadores.pipe(takeUntil(this._unsubscribeAll)).subscribe({ next: (c) => this.configuracao1Form.get('numero_jogadores')?.setValue(c) });
-		this._gameService._numero_vidas.pipe(takeUntil(this._unsubscribeAll)).subscribe({ next: (c) => this.configuracao1Form.get('numero_vidas')?.setValue(c) });
-		this._gameService._dinheiro_inicial.pipe(takeUntil(this._unsubscribeAll)).subscribe({ next: (c) => this.configuracao1Form.get('dinheiro_inicial')?.setValue(c) });
-		this._gameService._custo_golpe_estado.pipe(takeUntil(this._unsubscribeAll)).subscribe({ next: (c) => this.configuracao1Form.get('custo_golpe_estado')?.setValue(c) });
-		this._gameService._golpe_estado_obrigatorio.pipe(takeUntil(this._unsubscribeAll)).subscribe({ next: (c) => this.configuracao1Form.get('golpe_estado_obrigatorio')?.setValue(c) });
-
-		this.configuracao2Form = this._formBuilder.group({
-			nome_j0: [''],
-			nome_j1: [''],
-			nome_j2: [''],
-			nome_j3: [''],
-			nome_j4: [''],
-			nome_j5: [''],
-			nome_j6: [''],
-			nome_j7: [''],
-			nome_j8: [''],
-			nome_j9: [''],
-		});
+		this._gameService._numero_jogadores.pipe(takeUntil(this._unsubscribeAll)).subscribe({ next: (v) => this.configuracaoForm.get('numero_jogadores')?.setValue(v) });
+		this._gameService._numero_vidas.pipe(takeUntil(this._unsubscribeAll)).subscribe({ next: (v) => this.configuracaoForm.get('numero_vidas')?.setValue(v) });
+		this._gameService._dinheiro_inicial.pipe(takeUntil(this._unsubscribeAll)).subscribe({ next: (v) => this.configuracaoForm.get('dinheiro_inicial')?.setValue(v) });
+		this._gameService._custo_golpe_estado.pipe(takeUntil(this._unsubscribeAll)).subscribe({ next: (v) => this.configuracaoForm.get('custo_golpe_estado')?.setValue(v) });
+		this._gameService._golpe_estado_obrigatorio.pipe(takeUntil(this._unsubscribeAll)).subscribe({ next: (v) => this.configuracaoForm.get('golpe_estado_obrigatorio')?.setValue(v) });
 	}
 
 	ngOnDestroy(): void {
@@ -70,50 +45,25 @@ export class ConfigPage implements OnInit, OnDestroy {
 	}
 
 	aumentarValor(campo: string) {
-		this.configuracao1Form.get(campo)?.setValue(this.configuracao1Form.get(campo)?.value + 1);
+		this.configuracaoForm.get(campo)?.setValue(this.configuracaoForm.get(campo)?.value + 1);
 	}
 
 	diminuirValor(campo: string) {
-		if (this.configuracao1Form.get(campo)?.value - 1 >= 0) this.configuracao1Form.get(campo)?.setValue(this.configuracao1Form.get(campo)?.value - 1);
+		if (this.configuracaoForm.get(campo)?.value - 1 >= 0) this.configuracaoForm.get(campo)?.setValue(this.configuracaoForm.get(campo)?.value - 1);
 	}
 
-	qtdJogadores() {
-		return new Array(this.configuracao1Form.get('numero_jogadores')?.value);
-	}
+	voltarHome = () => this._route.navigateByUrl('');
 
-	configurarJogadores() {
-		let jogadores: Jogador[] = [];
-		for (let i = 0; i < this.configuracao1Form.get('numero_jogadores')?.value; i++) {
-			jogadores = [...jogadores, { nome: this.configuracao2Form.get(`nome_j${i}`)?.value.trim(), vida: this.configuracao1Form.get('numero_vidas')?.value, dinheiro: this.configuracao1Form.get('dinheiro_inicial')?.value }];
-		}
-		this._gameService._jogadores.next(jogadores);
-		this._changeDetectorsRef.markForCheck();
-	}
-
-	voltarHome() {
-		this._route.navigate(['']);
-	}
+	resetarConfiguracao = () => this._gameService.resetarConfiguracao();
 
 	enviarConfiguracao() {
-		if (this.fase_conf === 0) {
-			if (this.configuracao1Form.valid) {
-				this._gameService._numero_jogadores.next(this.configuracao1Form.get('numero_jogadores')?.value);
-				this._gameService._numero_vidas.next(this.configuracao1Form.get('numero_vidas')?.value);
-				this._gameService._dinheiro_inicial.next(this.configuracao1Form.get('dinheiro_inicial')?.value);
-				this._gameService._custo_golpe_estado.next(this.configuracao1Form.get('custo_golpe_estado')?.value);
-				this._gameService._golpe_estado_obrigatorio.next(this.configuracao1Form.get('golpe_estado_obrigatorio')?.value);
-				this.fase_conf++;
-				this._changeDetectorsRef.markForCheck();
-			}
-		} else if (this.fase_conf === 1) {
-			let conf2_val = true;
-			for (let i = 0; i < this.configuracao1Form.get('numero_jogadores')?.value; i++) {
-				if (this.configuracao2Form.get(`nome_j${i}`)?.value.trim() === '') conf2_val = false;
-			}
-			if (conf2_val) {
-				this.configurarJogadores();
-				this._route.navigateByUrl('players');
-			}
+		if (this.configuracaoForm.valid) {
+			this._gameService._numero_jogadores.next(this.configuracaoForm.get('numero_jogadores')?.value);
+			this._gameService._numero_vidas.next(this.configuracaoForm.get('numero_vidas')?.value);
+			this._gameService._dinheiro_inicial.next(this.configuracaoForm.get('dinheiro_inicial')?.value);
+			this._gameService._custo_golpe_estado.next(this.configuracaoForm.get('custo_golpe_estado')?.value);
+			this._gameService._golpe_estado_obrigatorio.next(this.configuracaoForm.get('golpe_estado_obrigatorio')?.value);
+			this._route.navigateByUrl('players');
 		}
 	}
 }
--- src/app/pages/players/players.page.ts
+++ src/app/pages/players/players.page.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { IonContent, IonHeader, IonIcon, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { AlertController, IonicModule } from '@ionic/angular';
 import { Subject, takeUntil } from 'rxjs';
 import { GameService } from 'src/app/core/game.service';
 import { Jogador } from 'src/app/core/game.type';
@@ -11,27 +11,37 @@ import { Jogador } from 'src/app/core/game.type';
 	selector: 'app-players',
 	templateUrl: 'players.page.html',
 	standalone: true,
-	changeDetection: ChangeDetectionStrategy.OnPush,
-	imports: [IonIcon, IonHeader, IonToolbar, IonTitle, IonContent, CommonModule, ReactiveFormsModule],
+	imports: [IonicModule, CommonModule, ReactiveFormsModule],
 })
-export class PlayersPage implements OnDestroy {
+export class PlayersPage implements OnInit, OnDestroy {
 	private _unsubscribeAll: Subject<any> = new Subject<any>();
 
-	texto_rodape = '';
-	jogadores: Jogador[] | null = [];
+	jogadoresForm!: UntypedFormGroup;
+	form_iniciado: boolean = false;
+
+	numero_jogadores: number = 0;
+	numero_vidas: number = 0;
+	dinheiro_inicial: number = 0;
 
 	constructor(
-		private _changeDetectorsRef: ChangeDetectorRef,
 		private _router: Router,
 		private _gameService: GameService,
+		private _formBuilder: UntypedFormBuilder,
+		private _alertController: AlertController,
 	) {
-		_gameService._jogadores.pipe(takeUntil(this._unsubscribeAll)).subscribe({
-			next: (j) => {
-				this.jogadores = j;
-				_changeDetectorsRef.markForCheck();
+		_gameService._numero_jogadores.pipe(takeUntil(this._unsubscribeAll)).subscribe({
+			next: (v) => {
+				this.form_iniciado = false;
+				this.numero_jogadores = v;
+				this.iniciarFormGroup();
 			},
 		});
-		this.texto_rodape = _gameService.texto_rodape;
+		_gameService._numero_vidas.pipe(takeUntil(this._unsubscribeAll)).subscribe({ next: (v) => (this.numero_vidas = v) });
+		_gameService._dinheiro_inicial.pipe(takeUntil(this._unsubscribeAll)).subscribe({ next: (v) => (this.dinheiro_inicial = v) });
+	}
+
+	ngOnInit(): void {
+		this.iniciarFormGroup();
 	}
 
 	ngOnDestroy(): void {
@@ -39,19 +49,56 @@ export class PlayersPage implements OnDestroy {
 		this._unsubscribeAll.complete();
 	}
 
-	backButton() {
-		this._router.navigateByUrl('config');
+	iniciarFormGroup() {
+		this.jogadoresForm = this._formBuilder.group({});
+		for (let i = 0; i < this.numero_jogadores; i++) this.jogadoresForm.addControl(`nome_jog_${i}`, this._formBuilder.control(''));
+		this.form_iniciado = true;
 	}
 
-	randomizarPosicoes() {
-		if (this.jogadores) {
-			const pre_jogads: Jogador[] | null = this.jogadores.sort(() => Math.random() - 0.5);
-			this._gameService._jogadores.next(pre_jogads);
+	voltarConfig = () => this._router.navigateByUrl('config');
+
+	quantidadeJogadores() {
+		return new Array(this.numero_jogadores);
+	}
+
+	embaralharJogadores() {
+		if (this.jogadoresForm.value) {
+			let pre_jog: string[] = [];
+			for (let i = 0; i < this.numero_jogadores; i++) pre_jog.push(this.jogadoresForm.get(`nome_jog_${i}`)?.value);
+			pre_jog = pre_jog.sort(() => Math.random() - 0.5);
+			for (let i = 0; i < this.numero_jogadores; i++) this.jogadoresForm.get(`nome_jog_${i}`)?.setValue(pre_jog[i]);
 		}
 	}
 
-	jogar() {
-		this._gameService.resetarPartida();
-		this._router.navigateByUrl('game');
+	async mostrarAlerta() {
+		const alert = await this._alertController.create({
+			mode: 'ios',
+			backdropDismiss: false,
+			header: 'Campos em branco!',
+			message: 'Preencha todos os campos corretamente!',
+			buttons: ['OK'],
+		});
+		alert.present();
+	}
+
+	conferirJogadores(): boolean {
+		let tudo_ok = true;
+		for (let i = 0; i < this.numero_jogadores; i++) {
+			if (this.jogadoresForm.get(`nome_jog_${i}`)?.value.trim() === '') {
+				tudo_ok = false;
+				break;
+			}
+		}
+		return tudo_ok;
+	}
+
+	confirmarJogadores() {
+		if (this.conferirJogadores()) {
+			let jogadores: Jogador[] = [];
+			for (let i = 0; i < this.numero_jogadores; i++) jogadores.push({ nome: this.jogadoresForm.get(`nome_jog_${i}`)?.value.trim(), vida: this.numero_vidas, dinheiro: this.dinheiro_inicial });
+			this._gameService._jogadores.next(jogadores);
+			this._gameService.resetarPartida();
+			this._router.navigateByUrl('game');
+		} else this.mostrarAlerta();
 	}
 }
