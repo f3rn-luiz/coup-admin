5c2157afdb586e5d3cfa5dba6acbffa287cbe1f2
Luiz Fernando <f3rn.luiz@gmail.com>
2024-10-07 16:21:28 -0300

    fix(Route / Service): dados para teste estão comentados

--- src/app/core/game.service.ts
+++ src/app/core/game.service.ts
@@ -7,7 +7,7 @@ export class GameService {
 	public texto_rodape = 'By: Luiz F Souza - v1.3.0';
 
 	// FASE 1 - Básico da Partida
-	_numero_jogadores: BehaviorSubject<number> = new BehaviorSubject(10);
+	_numero_jogadores: BehaviorSubject<number> = new BehaviorSubject(2);
 	_numero_vidas: BehaviorSubject<number> = new BehaviorSubject(2);
 	_dinheiro_inicial: BehaviorSubject<number> = new BehaviorSubject(0);
 	_custo_golpe_estado: BehaviorSubject<number> = new BehaviorSubject(7);
@@ -15,46 +15,68 @@ export class GameService {
 
 	// FASE 2 - Jogadores e Posições
 	_jogadores: BehaviorSubject<Jogador[] | null> = new BehaviorSubject<Jogador[] | null>([
-		{ nome: 'Luiz', vida: 0, dinheiro: 7 },
-		{ nome: 'Bia', vida: 1, dinheiro: 5 },
-		{ nome: 'Xande', vida: 0, dinheiro: 3 },
-		{ nome: 'Tiago', vida: 0, dinheiro: 9 },
-		{ nome: 'Iago', vida: 2, dinheiro: 10 },
-		{ nome: 'Otavio', vida: 0, dinheiro: 4 },
-		{ nome: 'Gu', vida: 2, dinheiro: 4 },
-		{ nome: 'Matheus', vida: 2, dinheiro: 3 },
-		{ nome: 'Bianca', vida: 0, dinheiro: 10 },
-		{ nome: 'Daniel', vida: 0, dinheiro: 6 },
+		// { nome: 'Luiz', vida: 0, dinheiro: 7 },
+		// { nome: 'Bia', vida: 1, dinheiro: 5 },
+		// { nome: 'Xande', vida: 0, dinheiro: 3 },
+		// { nome: 'Tiago', vida: 0, dinheiro: 9 },
+		// { nome: 'Iago', vida: 2, dinheiro: 10 },
+		// { nome: 'Otavio', vida: 0, dinheiro: 4 },
+		// { nome: 'Gu', vida: 2, dinheiro: 4 },
+		// { nome: 'Matheus', vida: 2, dinheiro: 3 },
+		// { nome: 'Bianca', vida: 0, dinheiro: 10 },
+		// { nome: 'Daniel', vida: 0, dinheiro: 6 },
 	]);
 
 	// EM JOGO
 	_turno: BehaviorSubject<number> = new BehaviorSubject(0);
 	_rodada: BehaviorSubject<number> = new BehaviorSubject(1);
 	_historico_geral: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([
-		'Luiz cabeçudo',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
-		'Teste roliço',
+		// 'Luiz PERDEU 1 vida',
+		// 'Bia PERDEU 1 vida',
+		// 'Xande PERDEU 1 vida',
+		// 'Tiago PERDEU 1 vida',
+		// 'Iago RECEBEU $2',
+		// 'Otavio PERDEU 1 vida',
+		// 'Gu RECEBEU $2',
+		// 'Matheus RECEBEU $2',
+		// 'Bianca PERDEU 1 vida',
+		// 'Daniel PERDEU 1 vida',
+		// 'Luiz PERDEU 1 vida',
+		// 'Bia RECEBEU $3',
+		// 'Xande PERDEU 1 vida',
+		// 'Bianca RECEBEU $10',
+		// 'Tiago RECEBEU $2',
+		// 'Iago RECEBEU $3',
+		// 'Otavio PERDEU 1 vida',
+		// 'Gu RECEBEU $2',
+		// 'Matheus RECEBEU $1',
+		// 'Bianca PERDEU 1 vida',
+		// 'Daniel RECEBEU $2',
+		// 'Luiz PERDEU 1 vida',
+		// 'Bia RECEBEU $2',
+		// 'Xande PERDEU $2',
+		// 'Xande RECEBEU $3',
+		// 'Tiago RECEBEU $3',
+		// 'Iago RECEBEU $5',
+		// 'Otavio PERDEU 1 vida',
+		// 'Gu RECEBEU $2',
+		// 'Matheus RECEBEU $1',
+		// 'Daniel RECEBEU $2',
+		// 'Daniel RECEBEU $2',
+		// 'Daniel RECEBEU $2',
+		// 'Daniel RECEBEU $2',
+		// 'Daniel RECEBEU $2',
+		// 'Daniel RECEBEU $2',
+		// 'Daniel RECEBEU $2',
+		// 'Daniel RECEBEU $2',
+		// 'Daniel RECEBEU $2',
+		// 'Daniel RECEBEU $2',
+		// 'Daniel RECEBEU $2',
+		// 'Daniel RECEBEU $2',
+		// 'Daniel RECEBEU $2',
+		// 'Daniel RECEBEU $2',
+		// 'Daniel RECEBEU $2',
+		// 'Gu RECEBEU $2',
 	]);
 
 	constructor() {}
@@ -99,7 +121,7 @@ export class GameService {
 	resetarPartida() {
 		this._rodada.next(1);
 		this._turno.next(0);
-		this._historico_geral.next(['']);
+		this._historico_geral.next([]);
 	}
 
 	resetarTUDO() {
