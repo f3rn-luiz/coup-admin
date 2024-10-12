fab40be118e07639934cb838705767894355a3e6
Luiz Fernando <f3rn.luiz@gmail.com>
2024-10-12 11:25:56 -0300

    docs(Angular / Capacitor): browser to aplication build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
 	appId: 'com.luizfsouza.coupadmin',
 	appName: 'COUP Admin',
-	webDir: 'www',
+	webDir: 'www/browser',
 	plugins: {
 		SplashScreen: {
 			launchShowDuration: 3000,
