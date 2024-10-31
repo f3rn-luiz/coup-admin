bd0f1a3cc1baf6df284d0dbab89f044532c93736
Luiz Fernando <f3rn.luiz@gmail.com>
2024-10-31 11:47:02 -0300

    docs(App / Main): importação dos icones transferido para a Main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { PreloadAllModules, RouteReuseStrategy, provideRouter, withPreloading } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import * as allIcons from 'ionicons/icons';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+
+addIcons(allIcons);
 
 bootstrapApplication(AppComponent, {
 	providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideIonicAngular(), provideRouter(routes, withPreloading(PreloadAllModules))],
