3b210e83a0164c58c0bf5e26dc90b519b308cafc
Luiz Fernando <f3rn.luiz@gmail.com>
2024-11-01 17:54:39 -0300

    style(Game Service): histórico quando passa turno e remoção dos dados teste

--- src/app/core/game.service.ts
+++ src/app/core/game.service.ts
@@ -14,80 +14,28 @@ export class GameService {
 	_golpe_estado_obrigatorio: BehaviorSubject<number> = new BehaviorSubject(10);
 
 	// FASE 2 - Jogadores e Posições
-	_jogadores: BehaviorSubject<Jogador[] | null> = new BehaviorSubject<Jogador[] | null>([
-		// { nome: 'Luiz', vida: 0, dinheiro: 7 },
-		// { nome: 'Bia', vida: 1, dinheiro: 5 },
-		// { nome: 'Xande', vida: 0, dinheiro: 3 },
-		// { nome: 'Tiago', vida: 0, dinheiro: 9 },
-		// { nome: 'Iago', vida: 2, dinheiro: 10 },
-		// { nome: 'Otavio', vida: 0, dinheiro: 4 },
-		// { nome: 'Gu', vida: 2, dinheiro: 4 },
-		// { nome: 'Matheus', vida: 2, dinheiro: 3 },
-		// { nome: 'Bianca', vida: 0, dinheiro: 10 },
-		// { nome: 'Daniel', vida: 0, dinheiro: 6 },
-	]);
+	_jogadores: BehaviorSubject<Jogador[] | null> = new BehaviorSubject<Jogador[] | null>([]);
 
 	// EM JOGO
 	_turno: BehaviorSubject<number> = new BehaviorSubject(0);
 	_rodada: BehaviorSubject<number> = new BehaviorSubject(1);
-	_historico_geral: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([
-		// 'Gu RECEBEU $2',
-		// 'Daniel RECEBEU $2',
-		// 'Daniel RECEBEU $2',
-		// 'Daniel RECEBEU $2',
-		// 'Daniel RECEBEU $2',
-		// 'Daniel RECEBEU $2',
-		// 'Daniel RECEBEU $2',
-		// 'Daniel RECEBEU $2',
-		// 'Daniel RECEBEU $2',
-		// 'Daniel RECEBEU $2',
-		// 'Daniel RECEBEU $2',
-		// 'Daniel RECEBEU $2',
-		// 'Daniel RECEBEU $2',
-		// 'Daniel RECEBEU $2',
-		// 'Matheus RECEBEU $1',
-		// 'Gu RECEBEU $2',
-		// 'Otavio PERDEU 1 vida',
-		// 'Iago RECEBEU $5',
-		// 'Tiago RECEBEU $3',
-		// 'Xande RECEBEU $3',
-		// 'Xande PERDEU $2',
-		// 'Bia RECEBEU $2',
-		// 'Luiz PERDEU 1 vida',
-		// 'Daniel RECEBEU $2',
-		// 'Bianca PERDEU 1 vida',
-		// 'Matheus RECEBEU $1',
-		// 'Gu RECEBEU $2',
-		// 'Iago RECEBEU $3',
-		// 'Tiago RECEBEU $2',
-		// 'Bianca RECEBEU $10',
-		// 'Xande PERDEU 1 vida',
-		// 'Bia RECEBEU $3',
-		// 'Luiz PERDEU 1 vida',
-		// 'Daniel PERDEU 1 vida',
-		// 'Bianca PERDEU 1 vida',
-		// 'Otavio PERDEU 1 vida',
-		// 'Iago RECEBEU $2',
-		// 'Tiago PERDEU 1 vida',
-		// 'Xande PERDEU 1 vida',
-		// 'Bia PERDEU 1 vida',
-		// 'Luiz PERDEU 1 vida',
-	]);
+	_historico_geral: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
 
 	constructor() {}
 
-	registrarAcao(tipo: number, qtd: number, ganhou: boolean, jogador: number) {
-		// 0 - Vida / 1 - Dinheiro
+	registrarAcao(tipo: string, qtd: number, ganhou: boolean, jogador: number) {
+		// tipo = vida / turno / dinheiro
 		let mensagem = '';
 		let jogs = this._jogadores.getValue();
-		if (jogs && tipo === 0) mensagem = `${jogs[jogador].nome} PERDEU ${qtd} vida${qtd > 1 ? 's' : ''}`;
-		else if (jogs && tipo === 1) mensagem = `${jogs[jogador].nome} ${ganhou ? 'RECEBEU' : 'PERDEU'} $${qtd}`;
+		if (jogs && tipo === 'vida') mensagem = `${jogs[jogador].nome} perdeu ${qtd} vida${qtd > 1 ? 's' : ''}`;
+		else if (jogs && tipo === 'turno') mensagem = `${jogs[jogador].nome} usou uma carta`;
+		else if (jogs && tipo === 'dinheiro') mensagem = `${jogs[jogador].nome} ${ganhou ? 'recebeu' : 'perdeu'} $${qtd}`;
 		this._historico_geral.next([mensagem, ...this._historico_geral.getValue()]);
 
 		// AÇÕES
-		if (jogs && tipo === 0) {
+		if (jogs && tipo === 'vida') {
 			jogs[jogador].vida = jogs[jogador].vida - qtd;
-		} else if (jogs && tipo === 1) {
+		} else if (jogs && tipo === 'dinheiro') {
 			if (ganhou) jogs[jogador].dinheiro = jogs[jogador].dinheiro + qtd;
 			else if (jogs[jogador].dinheiro - qtd <= 0) jogs[jogador].dinheiro = 0;
 			else jogs[jogador].dinheiro = jogs[jogador].dinheiro - qtd;
